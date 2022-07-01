cc4b7cd391ecd761442d39ed80a22dd729ee1492
ANJAAAAN <anjanbasnet124@gmail.com>
2022-07-01 16:24:52 +0545

    first commit

---
+++ js/questions.js
@@ -0,0 +1,71 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "what is the sum of 2 + 2 ?",
+    answer: "4",
+    options: [
+      "4",
+      "5",
+      "3",
+      "2"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Solve for x: 4x + 9 = 12.",
+    answer: "3/4",
+    options: [
+      "1/2",
+      "4",
+      "4/3",
+      "3/4"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which of the following is equal to 56?",
+    answer: "99-43",
+    options: [
+      "73-18",
+      "80-25",
+      "79-24",
+      "99-43"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Simplify: (4 – 5) – (13 – 18 + 2).",
+    answer: "2",
+    options: [
+      "-1",
+      "2",
+      "-2",
+      "1"
+    ]
+  },
+    {
+    numb: 5,
+    question: "If we minus 712 from 1500, how much do we get?",
+    answer: "788",
+    options: [
+      "788",
+      "778",
+      "768",
+      "758"
+    ]
+  },
+
+
+  //   {
+  //   numb: 6,
+  //   question: " What is the sum of 130+125+191?",
+  //   answer: "446",
+  //   options: [
+  //     "335",
+  //     "456",
+  //     "446",
+  //     "426"
+  //   ]
+  // },
+];
